4a87e965370d26ded5a0fca5775fe8f4dcbe9e37
Ian Eure <ian.eure@gmail.com>
2012-05-15 10:22:03 -0700

    Bump version.

--- shell-here.el
+++ shell-here.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2009-2012  Ian Eure
 
 ;; Author: Ian Eure <ian.eure@gmail.com>
-;; Version: 1.0
+;; Version: 1.1
 ;; Keywords: unix, tools, processes
 
 ;; This program is free software; you can redistribute it and/or modify
